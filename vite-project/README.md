734999093f741e2964c63d310eda91fd7fd1fb55
Rafizito <148220157+Rafizito7@users.noreply.github.com>
2024-01-30 22:50:47 +0100

    kk

--- vite-project/README.md
+++ vite-project/README.md
@@ -6,3 +6,9 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+COMMENTAIRE :
+1. Pour nos données, nous avons créé des tableaux croisés dynamiques depuis Excel et nous les avons entrées sans fichier JSON.
+2. En ce qui concerne la treemap, Nivo ne nous permet pas de définir nos propres couleurs. Nous n'avons pas réussi à intégrer les logos des marques conformément à notre design, et nous aimerions savoir comment nous aurions pu afficher le nom des marques ou les logos à la place des valeurs.
+
+Cependant, les trois graphiques sont dynamiques.
--- vite-project/src/charts/treemapChart.jsx
+++ vite-project/src/charts/treemapChart.jsx
@@ -1,435 +1,104 @@
 import { ResponsiveTreeMap } from '@nivo/treemap'
-import React from 'react';
 
 
-const data = [
-  
-  {
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Étiquettes de lignes",
-    "01. Citadine": "Nombre de NATCODE",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Peugeot",
-    "01. Citadine": 1079,
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Renault",
-    "01. Citadine": "716",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Citroën",
-    "01. Citadine": "490",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Dacia",
-    "01. Citadine": "274",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Toyota",
-    "01. Citadine": "202",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Fiat",
-    "01. Citadine": "191",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Opel",
-    "01. Citadine": "118",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Audi",
-    "01. Citadine": "111",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Volkswagen",
-    "01. Citadine": "89",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Ford",
-    "01. Citadine": "68",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Mini",
-    "01. Citadine": "41",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Kia",
-    "01. Citadine": "34",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Nissan",
-    "01. Citadine": "31",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Seat",
-    "01. Citadine": "30",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Skoda",
-    "01. Citadine": "29",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Suzuki",
-    "01. Citadine": "27",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "DS",
-    "01. Citadine": "23",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Hyundai",
-    "01. Citadine": "20",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Smart",
-    "01. Citadine": "20",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "BMW",
-    "01. Citadine": "14",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Mitsubishi",
-    "01. Citadine": "6",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Abarth",
-    "01. Citadine": "5",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Honda",
-    "01. Citadine": "4",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Alfa Romeo",
-    "01. Citadine": "3",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Chevrolet",
-    "01. Citadine": "1",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Mazda",
-    "01. Citadine": "1",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "Total général",
-    "01. Citadine": "3627",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
-},
-{
-    "VEHICUL_CATEGORIE": "",
-    "01. Citadine": "",
-    "": ""
+
+const data = 
+{   
+    "children":
+     [
+      {
+        "name": "Peugeot",
+        "color": "rgb(#53AF6F)",
+        "loc": 1079
+      },
+      {
+        "name": "Renault",
+        "color": "hsl(331, 70%, 50%)",
+        "loc": 716
+      },
+      {
+        "name": "Citroën",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 490
+      },
+      {
+        "name": "Dacia",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 274
+      },
+      {
+        "name": "Toyota",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 202
+      },
+      {
+        "name": "Fiat",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 191
+      },
+      {
+        "name": "Opel",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 110
+      },
+      {
+        "name": "Audi",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 111
+      },
+      {
+        "name": "Volkswagen",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 89
+      },
+      {
+        "name": "Ford",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 68
+      },
+      {
+        "name": "Mini",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 41
+      },
+      {
+        "name": "Kia",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 34
+      },
+      {
+        "name": "Nissan",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 31
+      },
+      {
+        "name": "Seat",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 30
+      },
+      {
+        "name": "Skoda",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 29
+      },
+      {
+        "name": "Suzuki",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 27
+      },
+      {
+        "name": "Smart",
+        "color": "hsl(334, 70%, 50%)",
+        "loc": 20
+      }, 
+    ]
 }
-]
 const MyResponsiveTreeMap = () => (
     <ResponsiveTreeMap
         data={data}
-        keys={[
-            'peugeot',
-            'Renault',
-            'Citroën',
-        ]}
         identity="name"
         value="loc"
+        fill="color"
         valueFormat=".02s"
         tile="binary"
         outerPadding={9}
@@ -440,7 +109,7 @@ const MyResponsiveTreeMap = () => (
             modifiers: [
                 [
                     'darker',
-                    1.2
+                    3
                 ]
             ]
         }}
@@ -451,18 +120,18 @@ const MyResponsiveTreeMap = () => (
             modifiers: [
                 [
                     'darker',
-                    2
+                    1.2
                 ]
             ]
         }}
-        colors={{ scheme: 'yellow_green_blue' }}
-        nodeOpacity={1}
+        nodeOpacity={2}
+        colors={{ scheme: 'yellow_green' }}
         borderColor={{ 
             from: 'color',
             modifiers: [
                 [
                     'darker',
-                    0.1
+                    0
                 ]
             ]
         }}
